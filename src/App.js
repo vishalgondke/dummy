4cda23bf3ed54bd8a2adbff18ee94aea67ac47df
Vishal Gondke <vishalgondke@gmail.com>
2023-07-08 12:42:10 +0530

    made some changes to app.js

--- src/App.js
+++ src/App.js
@@ -20,17 +20,11 @@ export default function App() {
                     <Route path="/forget-password" component={ ForgetPasswordPage } />
                     <Route path="/home" component={ HomePage } />
                 </Switch>
-                <Footer />
             </div>
         </Router>
     )
 }
 
-const Footer = () => {
-    return (
-        <p className="text-center" style={ FooterStyle }>Designed & coded by <a href="https://izemspot.netlify.com" target="_blank" rel="noopener noreferrer">IZEMSPOT</a></p>
-    )
-}
 
 const FooterStyle = {
     background: "#222",
